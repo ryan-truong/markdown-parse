822ebcdf59615fae564f2684bbc6279a7a6a8c28
ryantruong25 <75468904+ryan-truong@users.noreply.github.com>
2022-01-26 15:45:09 -0800

    Revert "Add multiple test files"
    This reverts commit 996fa202fbc6fd5460336b4640caee2db4c47df0.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,12 +15,7 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(nextCloseBracket + 1 == openParen) {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            }
-            if(closeParen == -1) {
-                break;
-            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
         for(String s: toReturn){
